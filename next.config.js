42cf147bff5a1afd8f9442fcac28955d6c4a6c6d
voxy-ai22 <voxyai61@gmail.com>
2026-01-28 08:56:35 +0700

    feat: Update nodemailer and enable RSC for it
    Updates the nodemailer version to the latest compatible range in package.json and index.html.
    Enables server components to import nodemailer by adding it to experimental.serverComponentsExternalPackages in next.config.js. This is necessary for using nodemailer in Next.js server components.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    serverComponentsExternalPackages: ['nodemailer'],
+  },
 };
 
 module.exports = nextConfig;
